255390d3d8b9a21fe6074044cb66d45a41e8e04a
Eamonn Maguire <eamonnmag@gmail.com>
2014-12-15 19:24:25 +0000

    Initial commit

---
+++ footer.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+
+	</div><!-- #main -->
+<div class="clearfix"></div>
+
+<div id="footer" class="centralPanel">
+
+    <div id="footer_nav_wrapper">
+
+        <div class="col col_wide">
+             
+<img src="/wp-content/themes/isatools-tt/img/footer_logo.svg" alt="Logo" style="width: 40%"/>
+        </div>
+
+        <div class="col col_med">
+            <h3>Related Links</h3>
+            <ul>
+                <li><a href="http://www.oerc.ox.ac.uk" target="_blank">Oxford e-Research Centre</a></li>
+                <li><a href="http://www.ox.ac.uk" target="_blank">University of Oxford</a></li>
+            </ul>
+        </div>
+
+        <div class="col col_med">
+            <h3>Resources</h3>
+            <ul>
+                <li><a href="http://www.github.com/ovii" target="_blank">Code</a></li>
+                <li><a href="mailto:isatools@googlegroups.com">Contact us</a></li>
+            </ul>
+        </div>
+
+
+    </div>
+
+    <div id="footer_fineprint" align="center">
+        <br/>
+
+        ©2014-Present ISA-Tools | Designed by <a href="http://www.antarctic-design.co.uk" target="_blank"> Antarctic Design</a>.
+    </div>
+
+</div>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
