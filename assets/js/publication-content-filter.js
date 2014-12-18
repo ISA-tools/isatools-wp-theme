3ffb392c4682979e85992616ea556622ab422aaa
Eamonn Maguire <eamonnmag@gmail.com>
2014-12-18 14:03:28 +0000

    Updates

--- assets/js/publication-content-filter.js
+++ assets/js/publication-content-filter.js
@@ -5,8 +5,9 @@ function filter_publication_content(filterInputId, listId) {
     var valThis = $(filterInputId).val().toLowerCase();
     $(listId + '>div').each(function () {
         var text = $(this).text().toLowerCase().trim();
-        var ok_to_show = OUPF.check_if_ok_to_show(this);
+        var ok_to_show = true;
         ok_to_show ? (text.indexOf(valThis) != -1) ? $(this).fadeIn(500) : $(this).fadeOut(500) : $(this).fadeOut(200);
 
     });
 }
+
