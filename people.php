dacd71fd57acd7c42a9a0dee269b6f5cda380d5c
Eamonn Maguire <eamonnmag@gmail.com>
2014-12-18 18:25:02 +0000

    Updating people page

--- people.php
+++ people.php
@@ -92,7 +92,7 @@ if ( $the_query->have_posts() ) {
 /* Restore original Post Data */
 wp_reset_postdata();
 ?>
-</div>
+
 <div class="cf"></div><br/>
 		</div><!-- #content -->
 	</div><!-- #primary -->
