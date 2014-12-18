ee9d0a9008dd2c1e7fe3a1a26ea4e574b10069b7
Eamonn Maguire <eamonnmag@gmail.com>
2014-12-18 13:59:33 +0000

    Updates

---
+++ assets/js/publication-content-filter.js
@@ -0,0 +1,12 @@
+/**
+ * Created by eamonnmaguire on 18/12/14.
+ */
+function filter_publication_content(filterInputId, listId) {
+    var valThis = $(filterInputId).val().toLowerCase();
+    $(listId + '>div').each(function () {
+        var text = $(this).text().toLowerCase().trim();
+        var ok_to_show = OUPF.check_if_ok_to_show(this);
+        ok_to_show ? (text.indexOf(valThis) != -1) ? $(this).fadeIn(500) : $(this).fadeOut(500) : $(this).fadeOut(200);
+
+    });
+}
